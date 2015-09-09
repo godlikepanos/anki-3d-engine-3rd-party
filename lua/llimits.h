7ff196d28a76c53e49f387a60f8ebaac568ddee7
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2015-09-09 21:00:24 +0200

    Increase the LUA's userdata to 16

--- lua/llimits.h
+++ lua/llimits.h
@@ -47,7 +47,7 @@ typedef unsigned char lu_byte;
 
 /* type to ensure maximum alignment */
 #if !defined(LUAI_USER_ALIGNMENT_T)
-#define LUAI_USER_ALIGNMENT_T	union { double u; void *s; long l; }
+#define LUAI_USER_ALIGNMENT_T	union { double u; void *s; long l; } __attribute__((aligned(16)))
 #endif
 
 typedef LUAI_USER_ALIGNMENT_T L_Umaxalign;
