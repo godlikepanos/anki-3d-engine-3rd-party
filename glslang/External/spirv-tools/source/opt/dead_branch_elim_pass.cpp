4daa5790054d4e8229da1af47fff1b7a29ae7663
Panagiotis Christopoulos Charitos <godlike@ancient-ritual.com>
2018-01-26 22:21:58 +0200

    Try fixing the submodules 2

---
+++ glslang/External/spirv-tools/source/opt/dead_branch_elim_pass.cpp
@@ -0,0 +1,410 @@
+// Copyright (c) 2017 The Khronos Group Inc.
+// Copyright (c) 2017 Valve Corporation
+// Copyright (c) 2017 LunarG Inc.
+// Copyright (c) 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dead_branch_elim_pass.h"
+
+#include "cfa.h"
+#include "ir_context.h"
+#include "iterator.h"
+#include "make_unique.h"
+
+namespace spvtools {
+namespace opt {
+
+namespace {
+
+const uint32_t kBranchCondTrueLabIdInIdx = 1;
+const uint32_t kBranchCondFalseLabIdInIdx = 2;
+
+}  // anonymous namespace
+
+bool DeadBranchElimPass::GetConstCondition(uint32_t condId, bool* condVal) {
+  bool condIsConst;
+  ir::Instruction* cInst = get_def_use_mgr()->GetDef(condId);
+  switch (cInst->opcode()) {
+    case SpvOpConstantFalse: {
+      *condVal = false;
+      condIsConst = true;
+    } break;
+    case SpvOpConstantTrue: {
+      *condVal = true;
+      condIsConst = true;
+    } break;
+    case SpvOpLogicalNot: {
+      bool negVal;
+      condIsConst =
+          GetConstCondition(cInst->GetSingleWordInOperand(0), &negVal);
+      if (condIsConst) *condVal = !negVal;
+    } break;
+    default: { condIsConst = false; } break;
+  }
+  return condIsConst;
+}
+
+bool DeadBranchElimPass::GetConstInteger(uint32_t selId, uint32_t* selVal) {
+  ir::Instruction* sInst = get_def_use_mgr()->GetDef(selId);
+  uint32_t typeId = sInst->type_id();
+  ir::Instruction* typeInst = get_def_use_mgr()->GetDef(typeId);
+  if (!typeInst || (typeInst->opcode() != SpvOpTypeInt)) return false;
+  // TODO(greg-lunarg): Support non-32 bit ints
+  if (typeInst->GetSingleWordInOperand(0) != 32) return false;
+  if (sInst->opcode() == SpvOpConstant) {
+    *selVal = sInst->GetSingleWordInOperand(0);
+    return true;
+  } else if (sInst->opcode() == SpvOpConstantNull) {
+    *selVal = 0;
+    return true;
+  }
+  return false;
+}
+
+void DeadBranchElimPass::AddBranch(uint32_t labelId, ir::BasicBlock* bp) {
+  assert(get_def_use_mgr()->GetDef(labelId) != nullptr);
+  std::unique_ptr<ir::Instruction> newBranch(new ir::Instruction(
+      context(), SpvOpBranch, 0, 0,
+      {{spv_operand_type_t::SPV_OPERAND_TYPE_ID, {labelId}}}));
+  get_def_use_mgr()->AnalyzeInstDefUse(&*newBranch);
+  bp->AddInstruction(std::move(newBranch));
+}
+
+ir::BasicBlock* DeadBranchElimPass::GetParentBlock(uint32_t id) {
+  return context()->get_instr_block(get_def_use_mgr()->GetDef(id));
+}
+
+bool DeadBranchElimPass::MarkLiveBlocks(
+    ir::Function* func, std::unordered_set<ir::BasicBlock*>* live_blocks) {
+  std::unordered_set<ir::BasicBlock*> continues;
+  std::vector<ir::BasicBlock*> stack;
+  stack.push_back(&*func->begin());
+  bool modified = false;
+  while (!stack.empty()) {
+    ir::BasicBlock* block = stack.back();
+    stack.pop_back();
+
+    // Live blocks doubles as visited set.
+    if (!live_blocks->insert(block).second) continue;
+
+    uint32_t cont_id = block->ContinueBlockIdIfAny();
+    if (cont_id != 0) continues.insert(GetParentBlock(cont_id));
+
+    ir::Instruction* terminator = block->terminator();
+    uint32_t live_lab_id = 0;
+    // Check if the terminator has a single valid successor.
+    if (terminator->opcode() == SpvOpBranchConditional) {
+      bool condVal;
+      if (GetConstCondition(terminator->GetSingleWordInOperand(0u), &condVal)) {
+        live_lab_id = terminator->GetSingleWordInOperand(
+            condVal ? kBranchCondTrueLabIdInIdx : kBranchCondFalseLabIdInIdx);
+      }
+    } else if (terminator->opcode() == SpvOpSwitch) {
+      uint32_t sel_val;
+      if (GetConstInteger(terminator->GetSingleWordInOperand(0u), &sel_val)) {
+        // Search switch operands for selector value, set live_lab_id to
+        // corresponding label, use default if not found.
+        uint32_t icnt = 0;
+        uint32_t case_val;
+        terminator->ForEachInOperand(
+            [&icnt, &case_val, &sel_val, &live_lab_id](const uint32_t* idp) {
+              if (icnt == 1) {
+                // Start with default label.
+                live_lab_id = *idp;
+              } else if (icnt > 1) {
+                if (icnt % 2 == 0) {
+                  case_val = *idp;
+                } else {
+                  if (case_val == sel_val) live_lab_id = *idp;
+                }
+              }
+              ++icnt;
+            });
+      }
+    }
+
+    // Don't simplify branches of continue blocks. A path from the continue to
+    // the header is required.
+    // TODO(alan-baker): They can be simplified iff there remains a path to the
+    // backedge. Structured control flow should guarantee one path hits the
+    // backedge, but I've removed the requirement for structured control flow
+    // from this pass.
+    bool simplify = live_lab_id != 0 && !continues.count(block);
+
+    if (simplify) {
+      modified = true;
+      // Replace with unconditional branch.
+      // Remove the merge instruction if it is a selection merge.
+      AddBranch(live_lab_id, block);
+      context()->KillInst(terminator);
+      ir::Instruction* mergeInst = block->GetMergeInst();
+      if (mergeInst->opcode() == SpvOpSelectionMerge) {
+        context()->KillInst(mergeInst);
+      }
+      stack.push_back(GetParentBlock(live_lab_id));
+    } else {
+      // All successors are live.
+      block->ForEachSuccessorLabel([&stack, this](const uint32_t label) {
+        stack.push_back(GetParentBlock(label));
+      });
+    }
+  }
+
+  return modified;
+}
+
+void DeadBranchElimPass::MarkUnreachableStructuredTargets(
+    const std::unordered_set<ir::BasicBlock*>& live_blocks,
+    std::unordered_set<ir::BasicBlock*>* unreachable_merges,
+    std::unordered_map<ir::BasicBlock*, ir::BasicBlock*>*
+        unreachable_continues) {
+  for (auto block : live_blocks) {
+    if (auto merge_id = block->MergeBlockIdIfAny()) {
+      ir::BasicBlock* merge_block = GetParentBlock(merge_id);
+      if (!live_blocks.count(merge_block)) {
+        unreachable_merges->insert(merge_block);
+      }
+      if (auto cont_id = block->ContinueBlockIdIfAny()) {
+        ir::BasicBlock* cont_block = GetParentBlock(cont_id);
+        if (!live_blocks.count(cont_block)) {
+          (*unreachable_continues)[cont_block] = block;
+        }
+      }
+    }
+  }
+}
+
+bool DeadBranchElimPass::FixPhiNodesInLiveBlocks(
+    ir::Function* func, const std::unordered_set<ir::BasicBlock*>& live_blocks,
+    const std::unordered_map<ir::BasicBlock*, ir::BasicBlock*>&
+        unreachable_continues) {
+  bool modified = false;
+  for (auto& block : *func) {
+    if (live_blocks.count(&block)) {
+      for (auto iter = block.begin(); iter != block.end();) {
+        if (iter->opcode() != SpvOpPhi) {
+          break;
+        }
+
+        bool changed = false;
+        bool backedge_added = false;
+        ir::Instruction* inst = &*iter;
+        std::vector<ir::Operand> operands;
+        // Build a complete set of operands (not just input operands). Start
+        // with type and result id operands.
+        operands.push_back(inst->GetOperand(0u));
+        operands.push_back(inst->GetOperand(1u));
+        // Iterate through the incoming labels and determine which to keep
+        // and/or modify.
+        for (uint32_t i = 1; i < inst->NumInOperands(); i += 2) {
+          ir::BasicBlock* inc = GetParentBlock(inst->GetSingleWordInOperand(i));
+          auto cont_iter = unreachable_continues.find(inc);
+          if (cont_iter != unreachable_continues.end() &&
+              cont_iter->second == &block) {
+            // Replace incoming value with undef if this phi exists in the loop
+            // header. Otherwise, this edge is not live since the unreachable
+            // continue block will be replaced with an unconditional branch to
+            // the header only.
+            operands.emplace_back(
+                SPV_OPERAND_TYPE_ID,
+                std::initializer_list<uint32_t>{Type2Undef(inst->type_id())});
+            operands.push_back(inst->GetInOperand(i));
+            changed = true;
+            backedge_added = true;
+          } else if (live_blocks.count(inc) && inc->IsSuccessor(&block)) {
+            // Keep live incoming edge.
+            operands.push_back(inst->GetInOperand(i - 1));
+            operands.push_back(inst->GetInOperand(i));
+          } else {
+            // Remove incoming edge.
+            changed = true;
+          }
+        }
+
+        if (changed) {
+          uint32_t continue_id = block.ContinueBlockIdIfAny();
+          if (!backedge_added && continue_id != 0 &&
+              unreachable_continues.count(GetParentBlock(continue_id))) {
+            // Changed the backedge to branch from the continue block instead
+            // of a successor of the continue block. Add an entry to the phi to
+            // provide an undef for the continue block. Since the successor of
+            // the continue must also be unreachable (dominated by the continue
+            // block), any entry for the original backedge has been removed
+            // from the phi operands.
+            operands.emplace_back(
+                SPV_OPERAND_TYPE_ID,
+                std::initializer_list<uint32_t>{Type2Undef(inst->type_id())});
+            operands.emplace_back(SPV_OPERAND_TYPE_ID,
+                                  std::initializer_list<uint32_t>{continue_id});
+          }
+
+          // Either replace the phi with a single value or rebuild the phi out
+          // of |operands|.
+          //
+          // We always have type and result id operands. So this phi has a
+          // single source if there are two more operands beyond those.
+          if (operands.size() == 4) {
+            // First input data operands is at index 2.
+            uint32_t replId = operands[2u].words[0];
+            context()->ReplaceAllUsesWith(inst->result_id(), replId);
+            iter = context()->KillInst(&*inst);
+          } else {
+            // We've rewritten the operands, so first instruct the def/use
+            // manager to forget uses in the phi before we replace them. After
+            // replacing operands update the def/use manager by re-analyzing
+            // the used ids in this phi.
+            get_def_use_mgr()->EraseUseRecordsOfOperandIds(inst);
+            inst->ReplaceOperands(operands);
+            get_def_use_mgr()->AnalyzeInstUse(inst);
+            ++iter;
+          }
+        } else {
+          ++iter;
+        }
+      }
+    }
+  }
+
+  return modified;
+}
+
+bool DeadBranchElimPass::EraseDeadBlocks(
+    ir::Function* func, const std::unordered_set<ir::BasicBlock*>& live_blocks,
+    const std::unordered_set<ir::BasicBlock*>& unreachable_merges,
+    const std::unordered_map<ir::BasicBlock*, ir::BasicBlock*>&
+        unreachable_continues) {
+  bool modified = false;
+  for (auto ebi = func->begin(); ebi != func->end();) {
+    if (unreachable_merges.count(&*ebi)) {
+      // Make unreachable, but leave the label.
+      KillAllInsts(&*ebi, false);
+      // Add unreachable terminator.
+      ebi->AddInstruction(
+          MakeUnique<ir::Instruction>(context(), SpvOpUnreachable, 0, 0,
+                                      std::initializer_list<ir::Operand>{}));
+      ++ebi;
+      modified = true;
+    } else if (unreachable_continues.count(&*ebi)) {
+      // Make unreachable, but leave the label.
+      KillAllInsts(&*ebi, false);
+      // Add unconditional branch to header.
+      assert(unreachable_continues.count(&*ebi));
+      uint32_t cont_id = unreachable_continues.find(&*ebi)->second->id();
+      ebi->AddInstruction(
+          MakeUnique<ir::Instruction>(context(), SpvOpBranch, 0, 0,
+                                      std::initializer_list<ir::Operand>{
+                                          {SPV_OPERAND_TYPE_ID, {cont_id}}}));
+      get_def_use_mgr()->AnalyzeInstUse(&*ebi->tail());
+      ++ebi;
+      modified = true;
+    } else if (!live_blocks.count(&*ebi)) {
+      // Kill this block.
+      KillAllInsts(&*ebi);
+      ebi = ebi.Erase();
+      modified = true;
+    } else {
+      ++ebi;
+    }
+  }
+
+  return modified;
+}
+
+bool DeadBranchElimPass::EliminateDeadBranches(ir::Function* func) {
+  bool modified = false;
+  std::unordered_set<ir::BasicBlock*> live_blocks;
+  modified |= MarkLiveBlocks(func, &live_blocks);
+
+  std::unordered_set<ir::BasicBlock*> unreachable_merges;
+  std::unordered_map<ir::BasicBlock*, ir::BasicBlock*> unreachable_continues;
+  MarkUnreachableStructuredTargets(live_blocks, &unreachable_merges,
+                                   &unreachable_continues);
+  modified |= FixPhiNodesInLiveBlocks(func, live_blocks, unreachable_continues);
+  modified |= EraseDeadBlocks(func, live_blocks, unreachable_merges,
+                              unreachable_continues);
+
+  return modified;
+}
+
+void DeadBranchElimPass::Initialize(ir::IRContext* c) {
+  InitializeProcessing(c);
+
+  // Initialize extension whitelist
+  InitExtensions();
+};
+
+bool DeadBranchElimPass::AllExtensionsSupported() const {
+  // If any extension not in whitelist, return false
+  for (auto& ei : get_module()->extensions()) {
+    const char* extName =
+        reinterpret_cast<const char*>(&ei.GetInOperand(0).words[0]);
+    if (extensions_whitelist_.find(extName) == extensions_whitelist_.end())
+      return false;
+  }
+  return true;
+}
+
+Pass::Status DeadBranchElimPass::ProcessImpl() {
+  // Do not process if module contains OpGroupDecorate. Additional
+  // support required in KillNamesAndDecorates().
+  // TODO(greg-lunarg): Add support for OpGroupDecorate
+  for (auto& ai : get_module()->annotations())
+    if (ai.opcode() == SpvOpGroupDecorate) return Status::SuccessWithoutChange;
+  // Do not process if any disallowed extensions are enabled
+  if (!AllExtensionsSupported()) return Status::SuccessWithoutChange;
+  // Process all entry point functions
+  ProcessFunction pfn = [this](ir::Function* fp) {
+    return EliminateDeadBranches(fp);
+  };
+  bool modified = ProcessReachableCallTree(pfn, context());
+  return modified ? Status::SuccessWithChange : Status::SuccessWithoutChange;
+}
+
+DeadBranchElimPass::DeadBranchElimPass() {}
+
+Pass::Status DeadBranchElimPass::Process(ir::IRContext* module) {
+  Initialize(module);
+  return ProcessImpl();
+}
+
+void DeadBranchElimPass::InitExtensions() {
+  extensions_whitelist_.clear();
+  extensions_whitelist_.insert({
+      "SPV_AMD_shader_explicit_vertex_parameter",
+      "SPV_AMD_shader_trinary_minmax",
+      "SPV_AMD_gcn_shader",
+      "SPV_KHR_shader_ballot",
+      "SPV_AMD_shader_ballot",
+      "SPV_AMD_gpu_shader_half_float",
+      "SPV_KHR_shader_draw_parameters",
+      "SPV_KHR_subgroup_vote",
+      "SPV_KHR_16bit_storage",
+      "SPV_KHR_device_group",
+      "SPV_KHR_multiview",
+      "SPV_NVX_multiview_per_view_attributes",
+      "SPV_NV_viewport_array2",
+      "SPV_NV_stereo_view_rendering",
+      "SPV_NV_sample_mask_override_coverage",
+      "SPV_NV_geometry_shader_passthrough",
+      "SPV_AMD_texture_gather_bias_lod",
+      "SPV_KHR_storage_buffer_storage_class",
+      "SPV_KHR_variable_pointers",
+      "SPV_AMD_gpu_shader_int16",
+      "SPV_KHR_post_depth_coverage",
+      "SPV_KHR_shader_atomic_counter_ops",
+  });
+}
+
+}  // namespace opt
+}  // namespace spvtools
---
+++ glslang/External/spirv-tools/source/opt/insert_extract_elim.cpp
@@ -0,0 +1,294 @@
+// Copyright (c) 2017 The Khronos Group Inc.
+// Copyright (c) 2017 Valve Corporation
+// Copyright (c) 2017 LunarG Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "insert_extract_elim.h"
+
+#include "ir_context.h"
+#include "iterator.h"
+#include "spirv/1.2/GLSL.std.450.h"
+
+#include <vector>
+
+namespace spvtools {
+namespace opt {
+
+namespace {
+
+const uint32_t kExtractCompositeIdInIdx = 0;
+const uint32_t kInsertObjectIdInIdx = 0;
+const uint32_t kInsertCompositeIdInIdx = 1;
+const uint32_t kConstantValueInIdx = 0;
+const uint32_t kVectorShuffleVec1IdInIdx = 0;
+const uint32_t kVectorShuffleVec2IdInIdx = 1;
+const uint32_t kVectorShuffleCompsInIdx = 2;
+const uint32_t kTypeVectorCompTypeIdInIdx = 0;
+const uint32_t kTypeVectorLengthInIdx = 1;
+const uint32_t kTypeFloatWidthInIdx = 0;
+const uint32_t kExtInstSetIdInIdx = 0;
+const uint32_t kExtInstInstructionInIdx = 1;
+const uint32_t kFMixXIdInIdx = 2;
+const uint32_t kFMixYIdInIdx = 3;
+const uint32_t kFMixAIdInIdx = 4;
+
+}  // anonymous namespace
+
+bool InsertExtractElimPass::ExtInsMatch(const std::vector<uint32_t>& extIndices,
+                                        const ir::Instruction* insInst,
+                                        const uint32_t extOffset) const {
+  uint32_t numIndices = static_cast<uint32_t>(extIndices.size()) - extOffset;
+  if (numIndices != insInst->NumInOperands() - 2) return false;
+  for (uint32_t i = 0; i < numIndices; ++i)
+    if (extIndices[i + extOffset] != insInst->GetSingleWordInOperand(i + 2))
+      return false;
+  return true;
+}
+
+bool InsertExtractElimPass::ExtInsConflict(
+    const std::vector<uint32_t>& extIndices, const ir::Instruction* insInst,
+    const uint32_t extOffset) const {
+  if (extIndices.size() - extOffset == insInst->NumInOperands() - 2)
+    return false;
+  uint32_t extNumIndices = static_cast<uint32_t>(extIndices.size()) - extOffset;
+  uint32_t insNumIndices = insInst->NumInOperands() - 2;
+  uint32_t numIndices = std::min(extNumIndices, insNumIndices);
+  for (uint32_t i = 0; i < numIndices; ++i)
+    if (extIndices[i + extOffset] != insInst->GetSingleWordInOperand(i + 2))
+      return false;
+  return true;
+}
+
+bool InsertExtractElimPass::IsVectorType(uint32_t typeId) {
+  ir::Instruction* typeInst = get_def_use_mgr()->GetDef(typeId);
+  return typeInst->opcode() == SpvOpTypeVector;
+}
+
+uint32_t InsertExtractElimPass::DoExtract(ir::Instruction* compInst,
+                                          std::vector<uint32_t>* pExtIndices,
+                                          uint32_t extOffset) {
+  ir::Instruction* cinst = compInst;
+  uint32_t cid = 0;
+  uint32_t replId = 0;
+  while (true) {
+    if (cinst->opcode() == SpvOpCompositeInsert) {
+      if (ExtInsMatch(*pExtIndices, cinst, extOffset)) {
+        // Match! Use inserted value as replacement
+        replId = cinst->GetSingleWordInOperand(kInsertObjectIdInIdx);
+        break;
+      } else if (ExtInsConflict(*pExtIndices, cinst, extOffset)) {
+        // If extract has fewer indices than the insert, stop searching.
+        // Otherwise increment offset of extract indices considered and
+        // continue searching through the inserted value
+        if (pExtIndices->size() - extOffset < cinst->NumInOperands() - 2) {
+          break;
+        } else {
+          extOffset += cinst->NumInOperands() - 2;
+          cid = cinst->GetSingleWordInOperand(kInsertObjectIdInIdx);
+        }
+      } else {
+        // Consider next composite in insert chain
+        cid = cinst->GetSingleWordInOperand(kInsertCompositeIdInIdx);
+      }
+    } else if (cinst->opcode() == SpvOpVectorShuffle) {
+      // Get length of vector1
+      uint32_t v1_id = cinst->GetSingleWordInOperand(kVectorShuffleVec1IdInIdx);
+      ir::Instruction* v1_inst = get_def_use_mgr()->GetDef(v1_id);
+      uint32_t v1_type_id = v1_inst->type_id();
+      ir::Instruction* v1_type_inst = get_def_use_mgr()->GetDef(v1_type_id);
+      uint32_t v1_len =
+          v1_type_inst->GetSingleWordInOperand(kTypeVectorLengthInIdx);
+      // Get shuffle idx
+      uint32_t comp_idx = (*pExtIndices)[extOffset];
+      uint32_t shuffle_idx =
+          cinst->GetSingleWordInOperand(kVectorShuffleCompsInIdx + comp_idx);
+      // If undefined, give up
+      // TODO(greg-lunarg): Return OpUndef
+      if (shuffle_idx == 0xFFFFFFFF) break;
+      if (shuffle_idx < v1_len) {
+        cid = v1_id;
+        (*pExtIndices)[extOffset] = shuffle_idx;
+      } else {
+        cid = cinst->GetSingleWordInOperand(kVectorShuffleVec2IdInIdx);
+        (*pExtIndices)[extOffset] = shuffle_idx - v1_len;
+      }
+    } else if (cinst->opcode() == SpvOpExtInst &&
+               cinst->GetSingleWordInOperand(kExtInstSetIdInIdx) ==
+                   get_module()->GetExtInstImportId("GLSL.std.450") &&
+               cinst->GetSingleWordInOperand(kExtInstInstructionInIdx) ==
+                   GLSLstd450FMix) {
+      // If mixing value component is 0 or 1 we just match with x or y.
+      // Otherwise give up.
+      uint32_t comp_idx = (*pExtIndices)[extOffset];
+      std::vector<uint32_t> aIndices = {comp_idx};
+      uint32_t a_id = cinst->GetSingleWordInOperand(kFMixAIdInIdx);
+      ir::Instruction* a_inst = get_def_use_mgr()->GetDef(a_id);
+      uint32_t a_comp_id = DoExtract(a_inst, &aIndices, 0);
+      if (a_comp_id == 0) break;
+      ir::Instruction* a_comp_inst = get_def_use_mgr()->GetDef(a_comp_id);
+      if (a_comp_inst->opcode() != SpvOpConstant) break;
+      // If a value is not 32-bit, give up
+      uint32_t a_comp_type_id = a_comp_inst->type_id();
+      ir::Instruction* a_comp_type = get_def_use_mgr()->GetDef(a_comp_type_id);
+      if (a_comp_type->GetSingleWordInOperand(kTypeFloatWidthInIdx) != 32)
+        break;
+      uint32_t u = a_comp_inst->GetSingleWordInOperand(kConstantValueInIdx);
+      float* fp = reinterpret_cast<float*>(&u);
+      if (*fp == 0.0)
+        cid = cinst->GetSingleWordInOperand(kFMixXIdInIdx);
+      else if (*fp == 1.0)
+        cid = cinst->GetSingleWordInOperand(kFMixYIdInIdx);
+      else
+        break;
+    } else {
+      break;
+    }
+    cinst = get_def_use_mgr()->GetDef(cid);
+  }
+  // If search ended with CompositeConstruct or ConstantComposite
+  // and the extract has one index, return the appropriate component.
+  // TODO(greg-lunarg): Handle multiple-indices, ConstantNull, special
+  // vector composition, and additional CompositeInsert.
+  if (replId == 0 &&
+      (cinst->opcode() == SpvOpCompositeConstruct ||
+       cinst->opcode() == SpvOpConstantComposite) &&
+      (*pExtIndices).size() - extOffset == 1) {
+    uint32_t compIdx = (*pExtIndices)[extOffset];
+    // If a vector CompositeConstruct we make sure all preceding
+    // components are of component type (not vector composition).
+    uint32_t ctype_id = cinst->type_id();
+    ir::Instruction* ctype_inst = get_def_use_mgr()->GetDef(ctype_id);
+    if (ctype_inst->opcode() == SpvOpTypeVector &&
+        cinst->opcode() == SpvOpConstantComposite) {
+      uint32_t vec_comp_type_id =
+          ctype_inst->GetSingleWordInOperand(kTypeVectorCompTypeIdInIdx);
+      if (compIdx < cinst->NumInOperands()) {
+        uint32_t i = 0;
+        for (; i <= compIdx; i++) {
+          uint32_t compId = cinst->GetSingleWordInOperand(i);
+          ir::Instruction* componentInst = get_def_use_mgr()->GetDef(compId);
+          if (componentInst->type_id() != vec_comp_type_id) break;
+        }
+        if (i > compIdx) replId = cinst->GetSingleWordInOperand(compIdx);
+      }
+    } else {
+      replId = cinst->GetSingleWordInOperand(compIdx);
+    }
+  }
+  return replId;
+}
+
+bool InsertExtractElimPass::EliminateInsertExtract(ir::Function* func) {
+  bool modified = false;
+  for (auto bi = func->begin(); bi != func->end(); ++bi) {
+    ir::Instruction* inst = &*bi->begin();
+    while (inst) {
+      switch (inst->opcode()) {
+        case SpvOpCompositeExtract: {
+          uint32_t cid = inst->GetSingleWordInOperand(kExtractCompositeIdInIdx);
+          ir::Instruction* cinst = get_def_use_mgr()->GetDef(cid);
+          // Capture extract indices
+          std::vector<uint32_t> extIndices;
+          uint32_t icnt = 0;
+          inst->ForEachInOperand([&icnt, &extIndices](const uint32_t* idp) {
+            if (icnt > 0) extIndices.push_back(*idp);
+            ++icnt;
+          });
+          // Offset of extract indices being compared to insert indices.
+          // Offset increases as indices are matched.
+          uint32_t replId = DoExtract(cinst, &extIndices, 0);
+          if (replId != 0) {
+            const uint32_t extId = inst->result_id();
+            (void)context()->ReplaceAllUsesWith(extId, replId);
+            inst = context()->KillInst(inst);
+            modified = true;
+          } else {
+            inst = inst->NextNode();
+          }
+        } break;
+        default:
+          inst = inst->NextNode();
+          break;
+      }
+    }
+  }
+  return modified;
+}
+
+void InsertExtractElimPass::Initialize(ir::IRContext* c) {
+  InitializeProcessing(c);
+
+  // Initialize extension whitelist
+  InitExtensions();
+};
+
+bool InsertExtractElimPass::AllExtensionsSupported() const {
+  // If any extension not in whitelist, return false
+  for (auto& ei : get_module()->extensions()) {
+    const char* extName =
+        reinterpret_cast<const char*>(&ei.GetInOperand(0).words[0]);
+    if (extensions_whitelist_.find(extName) == extensions_whitelist_.end())
+      return false;
+  }
+  return true;
+}
+
+Pass::Status InsertExtractElimPass::ProcessImpl() {
+  // Do not process if any disallowed extensions are enabled
+  if (!AllExtensionsSupported()) return Status::SuccessWithoutChange;
+  // Process all entry point functions.
+  ProcessFunction pfn = [this](ir::Function* fp) {
+    return EliminateInsertExtract(fp);
+  };
+  bool modified = ProcessEntryPointCallTree(pfn, get_module());
+  return modified ? Status::SuccessWithChange : Status::SuccessWithoutChange;
+}
+
+InsertExtractElimPass::InsertExtractElimPass() {}
+
+Pass::Status InsertExtractElimPass::Process(ir::IRContext* c) {
+  Initialize(c);
+  return ProcessImpl();
+}
+
+void InsertExtractElimPass::InitExtensions() {
+  extensions_whitelist_.clear();
+  extensions_whitelist_.insert({
+      "SPV_AMD_shader_explicit_vertex_parameter",
+      "SPV_AMD_shader_trinary_minmax",
+      "SPV_AMD_gcn_shader",
+      "SPV_KHR_shader_ballot",
+      "SPV_AMD_shader_ballot",
+      "SPV_AMD_gpu_shader_half_float",
+      "SPV_KHR_shader_draw_parameters",
+      "SPV_KHR_subgroup_vote",
+      "SPV_KHR_16bit_storage",
+      "SPV_KHR_device_group",
+      "SPV_KHR_multiview",
+      "SPV_NVX_multiview_per_view_attributes",
+      "SPV_NV_viewport_array2",
+      "SPV_NV_stereo_view_rendering",
+      "SPV_NV_sample_mask_override_coverage",
+      "SPV_NV_geometry_shader_passthrough",
+      "SPV_AMD_texture_gather_bias_lod",
+      "SPV_KHR_storage_buffer_storage_class",
+      "SPV_KHR_variable_pointers",
+      "SPV_AMD_gpu_shader_int16",
+      "SPV_KHR_post_depth_coverage",
+      "SPV_KHR_shader_atomic_counter_ops",
+  });
+}
+
+}  // namespace opt
+}  // namespace spvtools
